40a7ac0698bbedf53fad6c293c70a742567a1415
Olivia Burgos Blanco <oliviaburgosblanco2023@gmail.com>
2023-02-18 12:03:17 -0500

    El index de la pagina del carrito

---
+++ assets/js/carrito.js
@@ -0,0 +1,20 @@
+// Carrito
+// #1 BASE DE DATOS
+
+// #2 Pintar los productos en el DOM
+
+// #3 Carrito
+
+// #4 agregar al carrito
+
+// #5 remover articulos
+
+// #6 Eliminar del carrito
+
+// #7 Contar Articulos
+
+// #8 El total
+
+// #9 Limpiar carrito
+
+// #10 Comparar
---
+++ assets/js/data/data.js
@@ -0,0 +1,76 @@
+const productos = [
+  {
+    id: 1,
+    nombre: 'Blusa Estampada Negra',
+    precio: 45.00,
+    imagen: './assets/img/blusaminnie.png',
+    categoria: 'Blusas',
+    cantidad: 50
+  },
+  {
+    id: 2,
+    nombre: 'Jumper Rino',
+    precio: 120.00,
+    imagen: './assets/img/jumperrino.png',
+    categoria: 'Pantalones',
+    cantidad: 70
+  },
+  {
+    id: 3,
+    nombre: 'Blusa Rosada',
+    precio: 76.00,
+    imagen: './assets/img/blusarosada.png',
+    categoria: 'Blusas',
+    cantidad: 130
+  },
+  {
+    id: 4,
+    nombre: 'Blusa Roja',
+    precio: 76.00,
+    imagen: './assets/img/blusaroja.png',
+    categoria: 'Blusas',
+    cantidad: 190
+  },
+  {
+    id: 5,
+    nombre: 'Jumper Conejo',
+    precio: 110.00,
+    imagen: './assets/img/jumperconejo.png',
+    categoria: 'Pantalones',
+    cantidad: 84
+  },
+  {
+    id: 6,
+    nombre: 'Vestido Azul',
+    precio: 140.00,
+    imagen: './assets/img/vestidoazul.png',
+    categoria: 'Vestidos',
+    cantidad: 120
+  },
+  {
+    id: 7,
+    nombre: 'Blusa Estampada Rosada ',
+    precio: 64.00,
+    imagen: './assets/img/blusarosaminnie.png',
+    categoria: 'Blusas',
+    cantidad: 87
+  },
+  {
+    id: 8,
+    nombre: 'Vestido blanco ',
+    precio: 15.00,
+    imagen: './assets/img/vestidoescarcha.png',
+    categoria: 'Vestidos',
+    cantidad: 60
+  },  
+  {
+    id: 9,
+    nombre: 'Braga Jean',
+    precio: 89.00,
+    imagen: './assets/img/braga.png',
+    categoria: 'Vestidos',
+    cantidad: 76
+  }
+]
+
+export default productos
